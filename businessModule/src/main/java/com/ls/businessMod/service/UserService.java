64392765a6804332410299265797e1b3f11b7c41
ladderStone <40949798+ladderStone@users.noreply.github.com>
2019-03-31 19:17:07 +0530

    Code to load user by email id
    Code to load user by email id

--- businessModule/src/main/java/com/ls/businessMod/service/UserService.java
+++ businessModule/src/main/java/com/ls/businessMod/service/UserService.java
@@ -2,7 +2,9 @@ package com.ls.businessMod.service;
 
 import java.util.List;
 
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 import com.ls.dataMod.model.User;
 
@@ -21,4 +23,6 @@ public interface UserService extends UserDetailsService{
 	public void deleteUser(User user);
 
 	public User loadUser(String email, String password);
+
+	public UserDetails loadUserByEmail(String email) throws UsernameNotFoundException;
 }
