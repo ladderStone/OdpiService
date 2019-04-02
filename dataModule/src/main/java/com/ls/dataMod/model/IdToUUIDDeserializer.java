5d2b4d688f322dcd12d01c093238ef3178e139b7
ladderStone <40949798+ladderStone@users.noreply.github.com>
2019-04-02 09:45:19 +0530

    Add files via upload
    support to UUID

---
+++ dataModule/src/main/java/com/ls/dataMod/model/IdToUUIDDeserializer.java
@@ -0,0 +1,17 @@
+package com.ls.dataMod.model;
+
+import java.io.IOException;
+import java.util.UUID;
+
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+
+public class IdToUUIDDeserializer extends JsonDeserializer<UUID> {
+	@Override
+	public UUID deserialize(JsonParser jp, DeserializationContext dc) throws IOException, JsonProcessingException {
+		return UUID.nameUUIDFromBytes(jp.getValueAsString().getBytes());
+	}
+}	 
+
